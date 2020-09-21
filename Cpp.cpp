9ed1ff65371fbc755ff222c41acb9210966f00df
Mojtaba Abedi <68202630+s-mojtaba-a@users.noreply.github.com>
2020-09-21 21:41:20 +0330

    Update Cpp.cpp

--- Cpp.cpp
+++ Cpp.cpp
@@ -9,6 +9,8 @@ void find_primes(vector<int> &s, int n)
         bool flag = 0;
         for (int j : s)
         {
+            if (j*j>i)
+                break;
             if (i % j == 0)
             {
                 flag = 1;
@@ -29,3 +31,26 @@ int main()
     vector<int> primes={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47};
     find_primes(primes,n); */
 }
+
+
+#### python 3.8.2 impelemenation ####
+
+def find_primes(s, n):
+    for i in range(51, n, 2):
+        flag = 0
+        for j in s:
+            if j*j > i:
+                break
+            if i % j == 0:
+                flag = 1
+                break
+        if not flag:
+            s.append(i)
+
+
+if __name__ == '__main__':
+    n = 1000
+    s = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    find_primes(s, n)
+    for i in s:
+        print(i, end=' ')
